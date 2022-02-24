a00e7c0bb5cb75b311ab07ff6745c77b99a55fb6
Alberto Rojas <ing.beto.rojas@gmail.com>
2022-02-24 18:58:10 -0500

    se agregó formik a marca

--- src/pages/Marcas/MarcaCreateOrUpdate.jsx
+++ src/pages/Marcas/MarcaCreateOrUpdate.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Formik } from 'formik';
 
 function CreateOrUpdateMarca({ onClose, id }) {
   return (
@@ -6,36 +7,48 @@ function CreateOrUpdateMarca({ onClose, id }) {
       <h2 className="font-semibold">
         {id === null || id === undefined ? 'Nueva' : 'Actualizar'} Marca
       </h2>
-      <form className="my-4">
-        <div className="flex flex-wrap -mx-3 mb-2">
-          <div className="w-full md:w-2/3 px-3 mb-6 md:mb-0">
-            <label
-              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-              htmlFor="grid-name"
-            >
-              Nombre
-            </label>
-            <input
-              className="input-box"
-              id="grid-name"
-              type="text"
-              placeholder="name"
-            />
-          </div>
-        </div>
-        <div className="flex justify-end mt-4">
-          <button type="button" className="btn btn-success">
-            Guardar
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger ml-2"
-            onClick={() => onClose()}
-          >
-            Cancelar
-          </button>
-        </div>
-      </form>
+      <Formik
+        initialValues={{
+          nombre: ''
+        }}
+        onSubmit={() => {}}
+      >
+        {(formik) => (
+          <form className="my-4" onSubmit={formik.handleChange}>
+            <div className="flex flex-wrap -mx-3 mb-2">
+              <div className="w-full md:w-2/3 px-3 mb-6 md:mb-0">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-name"
+                >
+                  Nombre
+                </label>
+                <input
+                  className="input-box"
+                  id="grid-name"
+                  type="text"
+                  name="nombre"
+                  value={formik.values.nombre}
+                  placeholder="name"
+                  onChange={formik.handleChange}
+                />
+              </div>
+            </div>
+            <div className="flex justify-end mt-4">
+              <button type="button" className="btn btn-success">
+                Guardar
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger ml-2"
+                onClick={() => onClose()}
+              >
+                Cancelar
+              </button>
+            </div>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 }
