35dc960547485b10d1f77840406d46b43cd63580
Alberto Rojas <ing.beto.rojas@gmail.com>
2022-03-21 13:36:44 -0500

    fix: arreglo de list gastos adicion de columna soporte

--- src/pages/Gastos/GastosList.jsx
+++ src/pages/Gastos/GastosList.jsx
@@ -45,7 +45,7 @@ function GastosList() {
   }
 
   const columns = useMemo(
-    () => ['Maquina', 'Descripcion', 'Valor', 'Acciones'],
+    () => ['Maquina', 'Descripcion', 'Valor', 'soporte', 'Acciones'],
     []
   );
 
@@ -78,7 +78,26 @@ function GastosList() {
                 <td>{item.maquina.nombre}</td>
                 <td>{item.descripcion}</td>
                 <td>{item.valor}</td>
-                <td className="flex items-center  justify-between">
+                <td className="flex justify-center">
+                  <button type="button">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+                      />
+                    </svg>
+                  </button>
+                </td>
+
+                <td className="items-center">
                   <ButtonEdit
                     onClick={() =>
                       setModal({
