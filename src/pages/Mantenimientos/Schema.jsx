5239782472059df2f27345b50f372d564522609f
Alberto Rojas <ing.beto.rojas@gmail.com>
2022-04-07 23:45:01 -0500

    FEAT:Se realizó vistas de list y create de mantenimientos

---
+++ src/pages/Mantenimientos/Schema.jsx
@@ -0,0 +1,28 @@
+import * as Yup from 'yup';
+
+export const validationMantenimiento = Yup.object().shape({
+  tipo: Yup.string().required('El tipo es requerido'),
+  proveedor: Yup.object().required('El proveedor es requerido'),
+  maquina: Yup.object().required('La maquina es requerida'),
+  horometro: Yup.number().required('El horometro es requerido'),
+  descripcion: Yup.string().required('La descripcion es requerida'),
+  modalidad: Yup.string().required('La modalidad es requerida'),
+  costo: Yup.number().required('El valor es requerido'),
+  soporte: Yup.mixed().when('edit', (edit) => {
+    if (!edit) {
+      return Yup.mixed().required('El soporte de la factura es requerido');
+    }
+  })
+});
+
+export const initialData = {
+  edit: false,
+  tipo: '',
+  proveedor: '',
+  maquina: '',
+  descripcion: '',
+  modalidad: '',
+  horometro: 0,
+  costo: 0,
+  soporte: ''
+};
