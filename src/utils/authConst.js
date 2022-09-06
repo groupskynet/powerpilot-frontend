4f87b28a94c4d6aa72ce5f3173f02b6e92283925
ccolon <c_colon10@hotmail.com>
2022-09-06 13:34:13 -0500

    feat:  implementacion de keycloack con aws

--- src/utils/authConst.js
+++ src/utils/authConst.js
@@ -1,9 +1,9 @@
 const CONFIGURATION = {
-  client_id: 'dar',
+  client_id: 'dar-web',
   redirect_uri: `${window.location.origin}/authentication/callback`,
   silent_redirect_uri: `${window.location.origin}/authentication/silent-callback`,
   scope: 'openid',
-  authority: 'http://localhost:8085/auth/realms/group-sckynet',
+  authority: 'http://3.21.165.104:8085/auth/realms/dar',
   refresh_time_before_tokens_expiration_in_second: 70,
   service_worker_relative_url: '/OidcServiceWorker.js',
   service_worker_only: false
