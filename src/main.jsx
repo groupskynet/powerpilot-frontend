4ed81d12042df27560ebb738a34955e43f0db41a
Alberto Rojas <ing.beto.rojas@gmail.com>
2022-11-04 15:39:31 -0500

    Se termina la vista de reporte por horometro

--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,23 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import NiceModal from '@ebay/nice-modal-react';
-import { ReactKeycloakProvider } from '@react-keycloak/web';
+// import { ReactKeycloakProvider } from '@react-keycloak/web';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
-import keycloak from './utils/keycloack';
-import OidcSecure from './components/OdicSecure';
+// import keycloak from './utils/keycloack';
+// import OidcSecure from './components/OdicSecure';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReactKeycloakProvider authClient={keycloak}>
-      <ChakraProvider>
-        <NiceModal.Provider>
-          <Router>
-            <OidcSecure>
-              <App />
-            </OidcSecure>
-          </Router>
-        </NiceModal.Provider>
-      </ChakraProvider>
-    </ReactKeycloakProvider>
+    {/* <ReactKeycloakProvider authClient={keycloak}> */}
+    <ChakraProvider>
+      <NiceModal.Provider>
+        <Router>
+          <App />
+        </Router>
+      </NiceModal.Provider>
+    </ChakraProvider>
+    {/* </ReactKeycloakProvider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
