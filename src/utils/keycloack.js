587631f1fb35627197e32d5f2818a4a6edb94fec
colonca <c_colon10@hotmail.com>
2022-11-11 14:23:33 -0500

    configuración keycloack

--- src/utils/keycloack.js
+++ src/utils/keycloack.js
@@ -1,9 +1,9 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://localhost:8085/auth',
-  realm: 'dar',
-  clientId: 'dar-web'
+  url: import.meta.VITE_KEYCLOACK_URL,
+  realm: import.meta.VITE_KEYCLOACK_REALM,
+  clientId: import.meta.VITE_KEYCLOACK_CLIENT_ID
 });
 
 export default keycloak;
