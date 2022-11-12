9811c5981ff10f3bfa065538b608ebacb5717704
colonca <c_colon10@hotmail.com>
2022-11-12 08:57:44 -0500

    fix: variables keycloack

--- src/utils/keycloack.js
+++ src/utils/keycloack.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'https://dar-keycloack.groupskynet.site',
+  url: 'https://dar-keycloack.groupskynet.site/auth',
   realm: 'dar',
   clientId: 'dar-web'
 });
