f0dca69ef2cdc90824a864de17f68b80d933b302
Camilo <djfint01@gmail.com>
2023-04-05 16:15:37 -0500

    feat: keycloack

--- src/utils/keycloack.js
+++ src/utils/keycloack.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://localhost:8085',
+  url: 'https://keycloak.groupskynet.site/auth',
   realm: 'dar',
   clientId: 'dar-web'
 });
