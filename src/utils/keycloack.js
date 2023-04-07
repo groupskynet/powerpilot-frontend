4244ac7f26b3ef7904fc6b459c9fbce1f5cfa57c
Camilo <djfint01@gmail.com>
2023-04-07 13:02:15 -0500

    fix: keycloak

--- src/utils/keycloack.js
+++ src/utils/keycloack.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'https://keycloak.groupskynet.site',
+  url: 'https://keycloak.groupskynet.site/auth',
   realm: 'dar',
   clientId: 'dar-web'
 });
